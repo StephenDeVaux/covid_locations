ced35a1074b61eb8e604be999e1610a8f1f4a22d
StephenDeVaux <60765528+StephenDeVaux@users.noreply.github.com>
2020-05-08 06:36:28 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ require('./src/db/mongoose')
 const ProcessNewCasesByLocation = require('./src/functions/processNewCasesByLocation')
 var CronJob = require('cron').CronJob
 const getCasesRouter = require('./src/routers/getCases')
-const port = process.env.PORT 
 
 const app = express()
+const port = process.env.PORT 
+
+app.use(express.json())
+app.use(getCasesRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-app.use(express.json())
-app.use(getCasesRouter)
 
     ProcessNewCasesByLocation('https://interactive.guim.co.uk/covidfeeds/victoria.json', 'VIC')
     ProcessNewCasesByLocation('https://interactive.guim.co.uk/covidfeeds/nsw.json', 'NSW')
